36cbbe7ba4863bb2f073c481ab63ee4ff6ab014d
Ellen Nascimento <ellen_vinicius@hotmail.com>
2026-08-24 17:10:48 -0300

    feat: configura estrutura inicial do backend Spring Boot

---
+++ backend/src/test/java/com/ellen/finsphere/FinsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ellen.finsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
