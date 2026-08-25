1a9b5cb8cbcec7dc37411ce12cbd6757ab30a4f2
Ellen Nascimento <ellen_vinicius@hotmail.com>
2026-08-25 12:57:39 -0300

    feat: adiciona persistencia com PostgreSQl e JPA

--- backend/src/main/java/com/ellen/finsphere/model/Transaction.java
+++ backend/src/main/java/com/ellen/finsphere/model/Transaction.java
@@ -1,16 +1,37 @@
 package com.ellen.finsphere.model;
 
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "transactions")
 public class Transaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false, precision = 15, scale = 2)
     private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionType type;
+
+    @Column(nullable = false)
     private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     public Transaction() {
     }
@@ -21,7 +42,8 @@ public class Transaction {
             BigDecimal amount,
             TransactionType type,
             LocalDate date,
-            Category category) {
+            Category category,
+            User user) {
 
         this.id = id;
         this.description = description;
@@ -29,6 +51,7 @@ public class Transaction {
         this.type = type;
         this.date = date;
         this.category = category;
+        this.user = user;
     }
 
     public Long getId() {
@@ -77,5 +100,13 @@ public class Transaction {
 
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
